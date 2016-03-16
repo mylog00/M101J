cfee0f61a66a31de84c1480420eadcf7df5160ad
mylog00 <mylog00@ya.ru>
2016-03-16 22:40:08 +0500

    Used Spark and Freemarker together;

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,12 +1,40 @@
 package com.mongodb;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Spark.get("/", (request, response) -> "Hello Spark");
+        final Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setClassForTemplateLoading(App.class, "/");
+        // Sets how errors will appear.
+        // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
+        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        Spark.get("/", (request, response) -> {
+            final StringWriter writer = new StringWriter();
+            try {
+                final Template template = configuration.getTemplate("hello.ftl");
+
+                final Map<String, Object> helloMap = new HashMap<String, Object>() {{
+                    put("name", "Freemarker");
+                }};
+                template.process(helloMap, writer);
+            } catch (IOException | TemplateException e) {
+                e.printStackTrace();
+            }
+            return writer;
+        });
     }
 }
